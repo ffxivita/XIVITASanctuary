367389f7f4b1132c5494236590ebd8ac4c65188c
darka <luca@ahd-creative.com>
2022-09-22 15:09:24 +0200

    Dalamud Pachager

---
+++ src/Utils/Common.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace XIVITAGuide.Utils
+{
+    /// <summary>
+    ///     Collezione di funzioni di varia utilità
+    /// </summary>
+    public class Common
+    {
+        public static void OpenBrowser(string url)
+        {
+            Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
+        }
+    }
+}
