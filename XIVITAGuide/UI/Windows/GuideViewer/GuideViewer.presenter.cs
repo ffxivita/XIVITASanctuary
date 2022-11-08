1d9209ce855d532eb8ed4add6678cdfc484f4a5c
DarkArtek <darkartek@gmail.com>
2022-11-08 12:14:49 +0100

    GuideViewer

---
+++ XIVITAGuide/UI/Windows/GuideViewer/GuideViewer.presenter.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Reflection;
+using Dalamud.Interface.Internal.Notifications;
+using Dalamud.Logging;
+using XIVITAGuide.Base;
+using XIVITAGuide.Localization;
+using XIVITAGuide.Managers;
+using XIVITAGuide.Types;
+using XIVITAGuide.Ui.Windows.Settings;
+
+namespace XIVITAGuide.UI.Windows.GuideViewer
+{
+    public sealed class GuideViewerPresenter : IDisposable
+    {
+        public GuideViewerPresenter() => PluginService.Framework.Update += this.OnFrameworkUpdate;
+
+        public void Dispose() => PluginService.Framework.Update -= this.OnFrameworkUpdate;
+
+        /// <summary>
+        ///     Toggles the settings window.
+        /// </summary>
+        internal static void ToggleSettingsWindow()
+        {
+            if (PluginService.WindowManager.WindowSystem.GetWindow(TWindowNames.Settings) is SettingsWindow window)
+            {
+                window.IsOpen ^= true;
+            }
+        }
+
+        /// <summary>
+        ///     Gets the current plugin version.
+        /// </summary>
+        internal static string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? TGenerics.Unkown;
+
+        /// <summary>
+        ///     Pulls the configuration from the plugin service.
+        /// </summary>
+        internal static Configuration Configuration => PluginService.Configuration;
+
+        /// <summary>
+        ///     The currently selected guide to show in the GuideViewer window.
+        /// </summary>
+        internal Guide? SelectedGuide;
+
+        /// <summary>
+        ///     Last auto-selected guide.
+        /// </summary>
+        private Guide? lastAutoSelectedGuide;
+
+        /// <summary>
+        ///     The current player territory, used to determine if the zone has changed.
+        /// </summary>
+        private uint currentTerritory;
+
+        /// <summary>
+        ///     Detect when the player has changed zones and update the guide viewer accordingly through the game framework update event.
+        /// </summary>
+        public void OnFrameworkUpdate(object? e)
+        {
+            var currentTerritory = PluginService.ClientState.TerritoryType;
+            if (currentTerritory != this.currentTerritory)
+            {
+                this.currentTerritory = currentTerritory;
+                var playerGuide = GuideManager.GetGuideForCurrentTerritory();
+                PluginLog.Debug($"GuideViewerPresenter(OnFrameworkUpdate): Player changed territory to {this.currentTerritory}. (Guide for Territory: {playerGuide?.Name ?? "None"})");
+
+                if (playerGuide != null && playerGuide?.Sections?.Count > 0)
+                {
+                    this.SelectedGuide = playerGuide;
+                    this.lastAutoSelectedGuide = playerGuide;
+                    if (PluginService.Configuration.Display.AutoToggleGuideForDuty)
+                    {
+                        if (PluginService.WindowManager.WindowSystem.GetWindow(TWindowNames.GuideViewer) is GuideViewerWindow window)
+                        {
+                            PluginLog.Debug($"GuideViewerPresenter(OnTerritoryChange): Toggling guide viewer to open and displaying guide for {playerGuide.Name}.");
+                            window.IsOpen = true;
+                        }
+                    }
+                    else if (PluginService.WindowManager.WindowSystem.GetWindow(TWindowNames.GuideViewer)?.IsOpen == false)
+                    {
+                        PluginService.PluginInterface.UiBuilder.AddNotification(TGuideViewer.GuideAvailableForDuty, PluginConstants.PluginName, NotificationType.Info);
+                    }
+                }
+
+                else if (playerGuide == null && this.lastAutoSelectedGuide != null && this.lastAutoSelectedGuide == this.SelectedGuide)
+                {
+                    this.SelectedGuide = null;
+                    if (PluginService.WindowManager.WindowSystem.GetWindow(TWindowNames.GuideViewer) is GuideViewerWindow window)
+                    {
+                        PluginLog.Debug($"GuideViewerPresenter(OnTerritoryChange): Toggling guide viewer to closed - no guide data found for territory and last auto-selected guide is the same as the current selected guide.");
+                        window.IsOpen = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ XIVITAGuide/UI/Windows/GuideViewer/GuideViewer.window.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Numerics;
+using Dalamud.Interface;
+using Dalamud.Interface.Components;
+using Dalamud.Interface.Windowing;
+using Dalamud.Utility;
+using ImGuiNET;
+using XIVITAGuide.Base;
+using XIVITAGuide.Localization;
+using XIVITAGuide.Types;
+using XIVITAGuide.Ui.ImGuiBasicComponents;
+using XIVITAGuide.UI.ImGuiFullComponents.GuideSection;
+
+namespace XIVITAGuide.UI.Windows.GuideViewer
+{
+    public sealed class GuideViewerWindow : Window, IDisposable
+    {
+        internal GuideViewerPresenter Presenter = new();
+
+        public GuideViewerWindow() : base(TWindowNames.GuideViewer)
+        {
+            this.Flags |= ImGuiWindowFlags.NoScrollbar;
+
+            this.Size = new Vector2(380, 420);
+            this.SizeCondition = ImGuiCond.FirstUseEver;
+
+            if (GuideViewerPresenter.Configuration.Display.PreventGuideViewerMovement)
+            {
+                this.Flags |= ImGuiWindowFlags.NoMove;
+            }
+
+            if (GuideViewerPresenter.Configuration.Display.PreventGuideViewerResize)
+            {
+                this.Flags |= ImGuiWindowFlags.NoResize;
+            }
+        }
+
+        public void Dispose() => this.Presenter.Dispose();
+
+        private static bool CanShowExtendedInfo(Guide guide) => ImGui.GetWindowWidth() - ImGui.CalcTextSize(TGuideViewer.GuideHeading(guide.Name)).X > 180;
+
+        /// <summary>
+        ///     Draws the guide viewer window.
+        /// </summary>
+        public override void Draw()
+        {
+            var guide = this.Presenter.SelectedGuide;
+
+            // No guide selected, show non-selected message.
+            if (guide == null)
+            {
+                ImGui.TextWrapped(TGuideViewer.NoGuideSelected);
+                return;
+            }
+
+            // Player does not have the guide unlocked and have the setting enabled to hide locked guides.
+            if (!guide.IsUnlocked() && GuideViewerPresenter.Configuration.Display.HideLockedGuides)
+            {
+                ImGui.TextWrapped(TGuideViewer.GuideNotUnlocked);
+                return;
+            }
+
+            // Show the guide.
+            Colours.TextWrappedColoured(Colours.Grey, TGuideViewer.GuideHeading(guide.Name));
+            if (CanShowExtendedInfo(guide))
+            { ImGui.SameLine(); this.DrawHeaderButtons(); }
+            ImGui.Separator();
+
+            // No guide sections for this guide, cannot show anything.
+            if (guide.Sections == null || guide.Sections.Count == 0 || !guide.IsSupported())
+            {
+                ImGui.TextWrapped(TGuideViewer.NoInfoAvailable);
+                return;
+            }
+            GuideSectionComponent.Draw(guide.Sections);
+        }
+
+        private void DrawHeaderButtons()
+        {
+            var guideWindowNoMove = GuideViewerPresenter.Configuration.Display.PreventGuideViewerMovement;
+            var guideWindowNoResize = GuideViewerPresenter.Configuration.Display.PreventGuideViewerResize;
+            var issueReportUrl = $"{PluginConstants.RepoUrl}/issues/new?labels=type+|+guide-problem&template=guide_issue_report.yaml&title=Guide+Issue+Report%3A+{this.Presenter?.SelectedGuide?.Name} (PluginVer. {GuideViewerPresenter.PluginVersion})";
+
+            ImGui.SameLine();
+            if (this.Presenter?.SelectedGuide != null)
+            {
+                ImGui.SetCursorPosX(ImGui.GetWindowWidth() - 160);
+                if (ImGuiComponents.IconButton(FontAwesomeIcon.Flag))
+                {
+                    Util.OpenLink(issueReportUrl);
+                }
+                Common.AddTooltip($"{TGuideViewer.ReportIssueWithGuide}");
+                ImGui.SameLine();
+            }
+            else
+            {
+                ImGui.SetCursorPosX(ImGui.GetWindowWidth() - 120);
+            }
+
+            if (ImGuiComponents.IconButton(guideWindowNoMove ? FontAwesomeIcon.Lock : FontAwesomeIcon.Unlock))
+            {
+                this.Flags ^= ImGuiWindowFlags.NoMove;
+                GuideViewerPresenter.Configuration.Display.PreventGuideViewerMovement ^= true;
+                GuideViewerPresenter.Configuration.Save();
+            }
+            Common.AddTooltip(guideWindowNoMove ? TGuideViewer.UnlockWindowMovement : TGuideViewer.LockWindowMovement);
+
+            ImGui.SameLine();
+            if (ImGuiComponents.IconButton(guideWindowNoResize ? FontAwesomeIcon.Compress : FontAwesomeIcon.Expand))
+            {
+                this.Flags ^= ImGuiWindowFlags.NoResize;
+                GuideViewerPresenter.Configuration.Display.PreventGuideViewerResize ^= true;
+                GuideViewerPresenter.Configuration.Save();
+            }
+            Common.AddTooltip(guideWindowNoResize ? TGuideViewer.LockWindowResize : TGuideViewer.UnlockWindowResize);
+
+            ImGui.SameLine();
+            if (ImGuiComponents.IconButton(FontAwesomeIcon.Cog))
+            {
+                GuideViewerPresenter.ToggleSettingsWindow();
+            }
+            Common.AddTooltip(TGuideViewer.ToggleSettingsWindow);
+        }
+    }
+
+}
