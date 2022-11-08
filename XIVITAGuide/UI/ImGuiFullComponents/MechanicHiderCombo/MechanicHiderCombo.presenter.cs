3f19339ae3435b769e9aed5f1d74199e16c6a1d9
DarkArtek <darkartek@gmail.com>
2022-11-08 10:00:24 +0100

    Mechanic Hider Combo

---
+++ XIVITAGuide/UI/ImGuiFullComponents/MechanicHiderCombo/MechanicHiderCombo.presenter.cs
@@ -0,0 +1,9 @@
+using XIVITAGuide.Base;
+
+namespace XIVITAGuide.UI.ImGuiFullComponents.MechanicHiderCombo
+{
+    public sealed class MechanicHiderComboPresenter
+    {
+        internal static Configuration Configuration => PluginService.Configuration;
+    }
+}
